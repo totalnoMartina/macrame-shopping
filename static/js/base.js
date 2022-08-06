daba39994df9287b5632d104949017a9957e638a
totalnoMartina <totalnobzvz@yahoo.com>
2022-08-06 22:58:26 +0000

    Changed colors of navbr to start with trying palettes

--- static/js/base.js
+++ static/js/base.js
@@ -2,7 +2,7 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.body.style.backgroundColor = "rgba(237, 158, 124, 0.61)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
