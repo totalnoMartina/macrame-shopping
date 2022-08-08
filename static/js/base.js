40efa0f44d9eedcc5e2d87e2c7db108a7821cb62
totalnoMartina <totalnobzvz@yahoo.com>
2022-08-08 21:03:12 +0000

    Arranged navbar with login/logout and add bag,management, account

--- static/js/base.js
+++ static/js/base.js
@@ -2,12 +2,12 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(237, 158, 124, 0.61)";
+    document.body.style.backgroundColor = "#e09f63";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "rgb(239, 147, 113)";
+    document.body.style.backgroundColor = "#e09f63";
 }
