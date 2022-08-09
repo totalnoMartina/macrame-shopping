8ecd13608653407ef9b7124065da90ab2fcc49fd
totalnoMartina <totalnobzvz@yahoo.com>
2022-08-09 20:12:22 +0000

    Add macrame model and adminview

--- macrames/admin.py
+++ macrames/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Macrame, Category
 
-# Register your models here.
+
+class MacrameAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('-price',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Macrame, MacrameAdmin)
+admin.site.register(Category, CategoryAdmin)
--- macrames/models.py
+++ macrames/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """ A category of the products with a user-friendly name """
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=250)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        """ A method to show name of the category """
+        return self.name
+
+    def get_friendly_name(self):
+        """ A method to show user-friendly name of the categories """
+        return self.friendly_name
+
+
+class Macrame(models.Model):
+    """ A class for macrame items """
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    image = models.ImageField(null=False, blank=False)
+    name = models.CharField(max_length=200, null=False, blank=False)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        """ A method to show the name of the product  """
+        return self.name
